c9d0bcd9f722e6e03a8959b24af18783380a9b72
Daniel Bandala <60808296+bandala420@users.noreply.github.com>
2022-10-14 23:45:44 -0500

    Add files via upload

---
+++ win2k/POSync/POSync/Program.cs
@@ -0,0 +1,23 @@
+﻿//POSync
+//GRUPO MONSERRAT SA DE CV
+//Windows service - Point of sale synchronizer
+using System.ServiceProcess;
+
+namespace POSync
+{
+    static class Program
+    {
+        public static ServiceBase POSyncService;
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+            POSyncService = new Service1
+            {
+                CanHandlePowerEvent = true
+            };
+            ServiceBase.Run(POSyncService);
+        }
+    }
+}
